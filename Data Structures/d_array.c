322b24bf0de58dfb1da942d5082675ee3a00c8e2
Ben Cooper <ben0cooper@gmail.com>
2020-03-07 00:39:53 -0500

    Added misc functions and started structures

---
+++ Data Structures/d_array.c
@@ -0,0 +1,38 @@
+#include "common.h"
+
+#define SCALE 2
+
+typedef struct {
+	char **array;
+	size_t elements;
+	size_t capacity;
+} d_array;
+
+
+d_array *d_array_create(size_t size)
+{
+	d_array *result;
+	
+	result = (d_array *) e_malloc(sizeof(d_array));
+	result->array = (char **) e_malloc(size);
+	result->elements = 0;
+	result->capacity = size;
+	
+	return result;
+}
+
+
+void d_array_insert(d_array *array, char *item)
+{
+	// checking if array is full
+	if (array->elements >= array->capacity)
+	{
+		// increase size
+		array->array = (char **) e_realloc(array->array, array->capacity * 2);
+		array->capacity *= 2;
+	}
+	
+	// inserting element
+	array->array[array->elements] = item;
+	array->elements++;
+}
---
+++ Misc/word_analyzer.c
@@ -0,0 +1,50 @@
+#include "misc.h"
+
+/* print probabilities of each lettter occuring in a random
+ word from the provided list.
+ Requires strings to be lower case and contain only
+ alphabetic characters */
+ 
+void print_probabilities(char **str, size_t n)
+{
+	// number of words with each letter
+	int letter_count[26]  = { 0 };
+	// bitmap to for the presence of each letter in the word
+	int letter_bitmap[26] = { 0 };
+	
+	size_t i;
+	size_t j;
+	size_t len;
+	int idx;
+	float chance;
+	
+	// through each word
+	for (i = 0; i < n; i++)
+	{
+		len = strlen(str[i]);
+		
+		// through each letter
+		for (j = 0; j < len; j++)
+		{
+			idx = str[i][j] - 97;
+			// setting letter in bitmap
+			letter_bitmap[idx] = 1;
+		}
+		
+		// adding bitmap to total word count
+		for (idx = 0; idx < 26; idx++)
+		{
+			letter_count[idx] += letter_bitmap[idx];
+		}
+		
+		// resetting letter bitmap
+		memset(letter_bitmap, 0, sizeof(letter_bitmap));
+	}
+	
+	// outputting results
+	for (idx = 0; idx < 26; idx++)
+	{
+		chance = (double) letter_count[idx] / n * 100;
+		printf("Letter: %c\tChance: %.2f%%\n", idx + 97, chance);
+	}
+}
