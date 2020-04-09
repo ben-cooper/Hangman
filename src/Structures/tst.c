1db8c8d9f0e9beca52cc00c019d4f291019b3c73
Ben Cooper <ben0cooper@gmail.com>
2020-04-09 10:38:54 -0400

    Made variable names more consistent
    -changed tree to root

--- src/Structures/tst.c
+++ src/Structures/tst.c
@@ -25,49 +25,49 @@ struct tst_node *tst_create(char *str, size_t idx, size_t len)
 	return result;
 }
 
-void tst_insert(struct tst_node *tree, char *str, size_t idx, size_t len)
+void tst_insert(struct tst_node *root, char *str, size_t idx, size_t len)
 {
-	if (str[idx] < tree->chr) {
+	if (str[idx] < root->chr) {
 		/* left subtree */
-		if (tree->left)
-			tst_insert(tree->left, str, idx, len);
+		if (root->left)
+			tst_insert(root->left, str, idx, len);
 		else
-			tree->left = tst_create(str, idx, len);
+			root->left = tst_create(str, idx, len);
 
-	} else if (str[idx] > tree->chr) {
+	} else if (str[idx] > root->chr) {
 		/* right subtree */
-		if (tree->right)
-			tst_insert(tree->right, str, idx, len);
+		if (root->right)
+			tst_insert(root->right, str, idx, len);
 		else
-			tree->right = tst_create(str, idx, len);
+			root->right = tst_create(str, idx, len);
 
 	} else {
 		/* base case last character */
 		if ((len - idx) == 1) {
-			tree->word = str;
+			root->word = str;
 		} else {
 			/* middle subtree */
-			if (tree->middle)
-				tst_insert(tree->middle, str, idx + 1, len);
+			if (root->middle)
+				tst_insert(root->middle, str, idx + 1, len);
 			else
-				tree->middle = tst_create(str, idx + 1, len);
+				root->middle = tst_create(str, idx + 1, len);
 		}
 	}
 }
 
-int tst_search(struct tst_node const *tree, char const *str, size_t idx,
+int tst_search(struct tst_node const *root, char const *str, size_t idx,
                size_t len)
 {
-	if (!tree)
+	if (!root)
 		return 0;
 
-	if (str[idx] < tree->chr)
-		return tree->left && tst_search(tree->left, str, idx, len);
-	else if (str[idx] > tree->chr)
-		return tree->right && tst_search(tree->right, str, idx, len);
+	if (str[idx] < root->chr)
+		return root->left && tst_search(root->left, str, idx, len);
+	else if (str[idx] > root->chr)
+		return root->right && tst_search(root->right, str, idx, len);
 	else
-		return (((len - idx) == 1) && tree->word) ||
-		       tst_search(tree->middle, str, idx + 1, len);
+		return (((len - idx) == 1) && root->word) ||
+		       tst_search(root->middle, str, idx + 1, len);
 }
 
 void tst_pattern_search(struct tst_node * root, char const *pattern,
@@ -96,19 +96,19 @@ void tst_pattern_search(struct tst_node * root, char const *pattern,
 	}
 }
 
-size_t tst_height(struct tst_node const *tree)
+size_t tst_height(struct tst_node const *root)
 {
 	size_t left_height;
 	size_t right_height;
 	size_t middle_height;
 	size_t result;
 
-	if (!tree)
+	if (!root)
 		return 0;
 
-	left_height = tst_height(tree->left) + 1;
-	right_height = tst_height(tree->right) + 1;
-	middle_height = tst_height(tree->middle) + 1;
+	left_height = tst_height(root->left) + 1;
+	right_height = tst_height(root->right) + 1;
+	middle_height = tst_height(root->middle) + 1;
 
 	result = left_height > right_height ? left_height : right_height;
 	result = result > middle_height ? result : middle_height;
@@ -116,17 +116,17 @@ size_t tst_height(struct tst_node const *tree)
 	return result;
 }
 
-void tst_destroy(struct tst_node *tree, int destroy_str)
+void tst_destroy(struct tst_node *root, int destroy_str)
 {
-	if (!tree)
+	if (!root)
 		return;
 
-	tst_destroy(tree->left, destroy_str);
-	tst_destroy(tree->right, destroy_str);
-	tst_destroy(tree->middle, destroy_str);
+	tst_destroy(root->left, destroy_str);
+	tst_destroy(root->right, destroy_str);
+	tst_destroy(root->middle, destroy_str);
 
 	if (destroy_str)
-		free(tree->word);
+		free(root->word);
 
-	free(tree);
+	free(root);
 }
--- src/Structures/tst.h
+++ src/Structures/tst.h
@@ -22,18 +22,18 @@ struct tst_node *tst_create(char *str, size_t idx, size_t len);
 
 /**
  * Inserts a new string into the ternary search tree
- * @param tree: pointer to the tree
+ * @param root: pointer to the tree
  * @param str: the string to insert
  * @param idx: the current position in the string (used in recursion)
  */
-void tst_insert(struct tst_node *tree, char *str, size_t idx, size_t len);
+void tst_insert(struct tst_node *root, char *str, size_t idx, size_t len);
 
 /**
  * Frees all memory used by the entire ternary search tree
- * @param tree: pointer to the tst to destroy
+ * @param root: pointer to the tst to destroy
  * @param destroy_str: flag, if set will free all strings in the tst nodes
  */
-void tst_destroy(struct tst_node *tree, int destroy_str);
+void tst_destroy(struct tst_node *root, int destroy_str);
 
 /**
  * Writes all matching words into the file descriptor
@@ -49,18 +49,18 @@ void tst_pattern_search(struct tst_node *root, char const *pattern, size_t idx,
 
 /**
  * Returns 1 if the word is present in the tree
- * @param tree: pointer to the tree
+ * @param root: pointer to the tree
  * @param str: the string to search for
  * @param idx: the current position of the string (used in recursion)
  * @param len: the length of the string
  */
-int tst_search(struct tst_node const *tree, char const *str, size_t idx,
+int tst_search(struct tst_node const *root, char const *str, size_t idx,
                size_t len);
 
 /**
  * Returns the height of the ternary search tree
- * @param tree: pointer to the tree
+ * @param root: pointer to the tree
  */
-size_t tst_height(struct tst_node const *tree);
+size_t tst_height(struct tst_node const *root);
 
 #endif
